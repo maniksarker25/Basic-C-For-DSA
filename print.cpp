004d80cecf4f9ad435fecb970e5dce9e82b1e370
manik <maniksarker652@gmail.com>
2024-11-17 00:19:22 +0600

    learn how to print in c++ program

---
+++ print.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+
+    // make new line with c
+    std::cout << "Print first line\n";
+
+    int x = 10;
+    char c = 'A';
+    double d = 32.5453;
+    // pint three variable in one line;
+    std::cout << x << " " << c << " " << d << std::endl; // here use std::endl for new line this is for c++
+    std::cout << "Print last line";
+    return 0;
+}
