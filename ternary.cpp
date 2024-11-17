2f9e22e9057e071cf0db6b925d0863e299b47b9c
manik <maniksarker652@gmail.com>
2024-11-17 19:29:46 +0600

    learn ternary

---
+++ ternary.cpp
@@ -0,0 +1,13 @@
+// learn ternary in cpp
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+
+    x % 2 == 0 ? cout << "Even" : cout << "Odd";
+
+    return 0;
+}
