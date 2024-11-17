8e04a995f7a856cb90c1b3fb3e57a45759665b40
manik <maniksarker652@gmail.com>
2024-11-17 19:24:33 +0600

    learn if else

---
+++ if_else.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int x;
+
+    cin >> x;
+
+    if (x % 2 == 0)
+    {
+        cout << "This is even number";
+    }
+    else
+    {
+        cout << "This is odd number";
+    }
+
+    return 0;
+}
