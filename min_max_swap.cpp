4e94a41d9163c89b7ee934037506a9e52f3525a4
manik <maniksarker652@gmail.com>
2024-11-17 20:00:32 +0600

    use min , max and swap function

---
+++ min_max_swap.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    // usign fuction -- min , max , swap
+
+    // print min
+    cout << min(a, b) << endl;
+
+    // print max---- need to use {} this when compare more then 2 number
+    cout << max({a, b, c}) << endl;
+
+    // swap using buil in function 
+
+    swap(a,b);
+
+    cout << a << endl << b <<endl;
+    return 0;
+}
