9e311cde2b5da75f337b7bfcd1cd3b4495cddcf8
manik <maniksarker652@gmail.com>
2024-11-20 00:21:27 +0600

    sort the array

---
+++ sorting.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    // sort(staringPoint,endingPoint);
+    // a = first index
+    // a + n => last index + 1
+    sort(a, a + n);
+    // sort(a+2,a+4);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
