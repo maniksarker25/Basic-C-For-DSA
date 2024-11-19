d2c52a01f15ce8df0e913265279f0f478f064a1e
manik <maniksarker652@gmail.com>
2024-11-20 00:23:39 +0600

    array sort descending order

--- sorting.cpp
+++ sorting.cpp
@@ -17,8 +17,11 @@ int main()
     // sort(staringPoint,endingPoint);
     // a = first index
     // a + n => last index + 1
-    sort(a, a + n);
+    sort(a, a + n); // ascending
     // sort(a+2,a+4);
+
+    // descending
+    // sort(a, a + n, greater<int>());
 
     for (int i = 0; i < n; i++)
     {
