af656eaa71ca4e70d2be254f869a3744d0cdec46
manik <maniksarker652@gmail.com>
2024-11-19 22:25:55 +0600

    learn about this keyword and arrow sign

---
+++ this_arrow_sign.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Student
+{
+public:
+    int roll;
+    int cls;
+    double gpa;
+
+    // use constructor
+    Student(int roll, int cls, double gpa)
+    {
+        // (*this).roll = roll; here this is not a object , this is pointer that point tuli , tula etc;
+        // so that why here when i acess filed then make it arrow ,
+        /// if we do not use arrow then need to dereference the this--------------
+        (*this).roll = roll;
+        // shortcut is (=>);
+        // this->roll = roll;
+        this->cls = cls;
+        this->gpa = gpa;
+    }
+};
+
+int main()
+{
+
+    Student tuli(2, 2, 4.55);
+    Student tula(5, 5, 5);
+    cout << tuli.roll << " " << tuli.cls << " " << tuli.gpa << endl;
+    cout << tula.roll << " " << tula.cls << " " << tula.gpa << endl;
+
+    return 0;
+}
