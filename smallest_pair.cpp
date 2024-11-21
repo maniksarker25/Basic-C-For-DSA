8a9834ebfd770807b0ec1c7369e398ab4e94990d
manik <maniksarker652@gmail.com>
2024-11-22 01:26:52 +0600

    solve smaller pair problem

---
+++ smallest_pair.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        int sml = INT_MAX;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                int result = a[i] + a[j] + (j - i);
+                sml = min(sml, result);
+            }
+        }
+
+        cout << sml << endl;
+    }
+
+    return 0;
+}
