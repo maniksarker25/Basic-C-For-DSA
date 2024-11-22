c8cd2fc5a6fc96847f8b747fa9263149b826026a
manik <maniksarker652@gmail.com>
2024-11-22 11:23:06 +0600

    copy object and delete dynamic object

---
+++ copy_object_value.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Cricketer
+{
+public:
+    int jersey_no;
+    char country[100];
+};
+
+int main()
+{
+    Cricketer *dhoni = new Cricketer;
+    Cricketer *kholi = new Cricketer;
+    dhoni->jersey_no = 200;
+    char country_name[100] = "India";
+    strcpy(dhoni->country, country_name);
+
+    // kholi = dhoni;
+    // kholi->country = dhoni->country;
+    strcpy(kholi->country,dhoni->country);
+    kholi->jersey_no =dhoni->jersey_no;
+    delete dhoni;
+
+    cout << kholi->country << " " << kholi->jersey_no << endl;
+
+    return 0;
+}
