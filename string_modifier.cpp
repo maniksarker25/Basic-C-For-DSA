684b5c3b7780919b75aaf07f8f04b00cc1b2b313
manik <maniksarker652@gmail.com>
2024-11-23 22:37:10 +0600

    learn more string modifier like replace,erase,assign,insert etc

--- string_modifier.cpp
+++ string_modifier.cpp
@@ -10,12 +10,25 @@ int main()
     string s2 = " Hi";
     // s += s2;
     // s.append(s2);      // both are same
-    cout << s << endl; // Hello World Hi
+    // cout << s << endl; // Hello World Hi
 
     // add a character to end
-    s.push_back('M');
+    // s.push_back('M');
     // remove last character
-    s.pop_back();
+    // s.pop_back();
+
+    // use =
+
+    // s = "Gello World";
+    // s.assign("Gello"); // same as ==
+
+    // s.erase(5); // start delete from 5 index
+    // s.erase(5, 2); // start with 5 and delete 2 character
+
+    // s.replace(6, 5, "Bangladesh"); // kothai replace korbo , okana thaka koita index delete korbo , ki bosata chai
+
+    // insert a portion to a specific position
+    s.insert(5, " Bangladesh"); // output : Hello Bangladesh World
 
     cout << s << endl;
 
