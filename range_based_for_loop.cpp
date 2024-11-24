ab26b0ef320731d302de9cea11cf696a713daccf
manik <maniksarker652@gmail.com>
2024-11-24 20:28:23 +0600

    learn range based for loop

---
+++ range_based_for_loop.cpp
@@ -0,0 +1,25 @@
+// range based for loop
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    string s;
+    cin >> s;
+
+    // for (int i = 0; i < s.size(); i++)
+    // {
+    //     cout << s[i] << endl;
+    // }
+
+    // short cut loop
+    for (char c : s)
+    {
+        cout << c << endl;
+    }
+
+    return 0;
+}
