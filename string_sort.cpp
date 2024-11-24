1161378612295655302275f55b64b13734da4d0d
manik <maniksarker652@gmail.com>
2024-11-24 20:21:21 +0600

    string sort using sort function

---
+++ string_sort.cpp
@@ -0,0 +1,17 @@
+// string sort
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    string s;
+    cin >> s;
+
+    // sort(starting,ending);
+    sort(s.begin(), s.end());
+    cout << s << endl;
+    return 0;
+}
