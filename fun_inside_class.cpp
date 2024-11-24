5e119f679b3954ad1faede6f0f7fe763d049f24a
manik <maniksarker652@gmail.com>
2024-11-24 23:13:44 +0600

    work with function in class

--- fun_inside_class.cpp
+++ fun_inside_class.cpp
@@ -9,10 +9,14 @@ class Student
 public:
     string name;
     int rol;
-    Student(string name, int rol)
+    int math;
+    int english;
+    Student(string name, int rol, int math, int english)
     {
         this->name = name;
         this->rol = rol;
+        this->math = math;
+        this->english = english;
     }
 
     // fun in class
@@ -20,13 +24,19 @@ public:
     {
         cout << "Hello from" << " " << name << endl;
     }
+
+    //
+    void total()
+    {
+        cout << "Total marks of " << name << math + english << endl;
+    }
 };
 
 int main()
 {
-    Student manik("Manik Sarker", 23);
+    Student manik("Manik Sarker", 23, 75, 99);
     manik.hello();
-    Student roton("Roton Sarker", 43);
-    roton.hello();
+    Student roton("Roton Sarker", 43, 87, 34);
+    roton.total();
     return 0;
 }
