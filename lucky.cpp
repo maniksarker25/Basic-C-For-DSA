27e1cff7ee25138dab64139dd34aced2b3f37320
manik <maniksarker652@gmail.com>
2024-11-25 22:07:49 +0600

    solve lucky digit

--- lucky.cpp
+++ lucky.cpp
@@ -1,5 +1,4 @@
-// lucky 
-
+// lucky
 
 #include <bits/stdc++.h>
 
@@ -7,8 +6,37 @@ using namespace std;
 
 int main()
 {
-  
-  
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        string s;
+        cin >> s;
+
+        int first_three_cout = 0;
+        int last_three_cout = 0;
+
+        for (int i = 0; i < 3; i++)
+        {
+            first_three_cout += s[i] - '0';
+        }
+
+        for (int i = 5; i > 2; i--)
+        {
+            last_three_cout += s[i] - '0';
+        }
+
+        if (first_three_cout == last_three_cout)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
 
     return 0;
 }
