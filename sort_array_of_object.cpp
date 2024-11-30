6765f15331d848d97171ab747c228eb978df4cbe
manik <maniksarker652@gmail.com>
2024-11-30 17:01:11 +0600

    successfully handle sort for array of object

--- sort_array_of_object.cpp
+++ sort_array_of_object.cpp
@@ -16,14 +16,51 @@ public:
 // compare function for sort
 bool cmp(Student l, Student r)
 {
-    if (l.marks < r.marks)
-    {
-        return true;
-    }
+    // if (l.marks < r.marks)
+    // {
+    //     return true;
+    // }
 
+    // else
+    // {
+    //     return false;
+    // }
+    // sort cut
+    // ascending order
+    // return l.marks < r.marks;
+    // desending order
+    // return l.marks > r.marks;
+    // if mark is same sort based on roll for those object
+    // if (l.marks > r.marks)
+    // {
+    //     return true;
+    // }
+    // else if (l.marks < r.marks)
+    // {
+    //     return false;
+    // }
+    // // when 2 object mark is same , then sort by roll
+    // else
+    // {
+    //     // if (l.roll < r.roll)
+    //     // {
+    //     //     return true;
+    //     // }
+    //     // else
+    //     // {
+    //     //     return false;
+    //     // }
+    //     return l.roll < r.roll;
+    // }
+
+    // sort cut
+    if (l.marks == r.marks)
+    {
+        return l.roll < r.roll;
+    }
     else
     {
-        return false;
+        return l.marks > r.marks;
     }
 }
 
